1b2ef142f0433325dabca8cf362d51a8de50ab81
wfderrick <wderrick@terpmail.umd.edu>
2026-01-31 20:25:56 -0500

    almost nothing

--- app/round.py
+++ app/round.py
@@ -1,7 +1,7 @@
 import tools
 import structs
 from opponentbrain import discard
-
+import copy
 def round(
     player1_hand, player2_hand, cut_card, round_num, player1_score, player2_score, crib
 ):
@@ -9,7 +9,7 @@ def round(
     player1_hand = tools.transform_cards(player1_hand, structs.Hand(0, 0, 0, 0, 0, 0))
     player2_hand = tools.transform_cards(player2_hand, structs.Hand(0, 0, 0, 0, 0, 0))
     cut_card = tools.get_card(cut_card, structs.Card(0, 0, 0))
-
+    last_player = 0
     print(
         "X```````````````````````````````````````````````````````````````````````````\n"
         f"X   Your Score: {player1_score}             Opponent's Score: {player2_score}             Goal: 120 \n"
@@ -27,8 +27,10 @@ def round(
     print(f"{player1_hand}\n")
     if round_num % 2 == 0:
         print("Opponents Crib!")
+        last_player = 2 
     else:
         print("Your Crib!")
+        last_player = 1
     val1 = input("Enter the first number to be discarded. ")
     while not (isinstance(val1, str) and val1.isdigit() and int(val1) > 0 and int(val1) < 7):
         val1 = input("Enter a value between 1 and 6. ")
@@ -57,4 +59,10 @@ def round(
     print(cut_card)
 
     empt = input("Press enter to begin the pegging round!")
-    
+    cur_val = 0
+    p1_pegging = copy.deepcopy(player1_hand.hand)
+    p2_pegging = copy.deepcopy(player2_hand.hand)
+
+    while len(player1_hand.hand) > 0 or len(player2_hand.hand) > 0:
+        if last_player == 1 and len(player2_hand.hand) > 0:
+            play = 
